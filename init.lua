cd96dd00d4ba69a8e1dac2d84d2302619ddb41f7
Akil Rammohan <akilan.rammohan@gmail.com>
2025-08-21 10:40:02 -0700

    copy highlight after yank autocmd from kickstart.nvim

--- init.lua
+++ init.lua
@@ -1,71 +1,85 @@
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.signcolumn = "yes"
+vim.o.signcolumn = 'yes'
 vim.o.wrap = false
 vim.o.tabstop = 2
 vim.o.termguicolors = true
 vim.o.timeoutlen = 600
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
-vim.keymap.set("n", "<leader>o", ":update<CR> :so<CR>")
-vim.keymap.set("n", "<leader>w", ":write<CR>")
-vim.keymap.set("n", "<leader>q", ":quit<CR>")
-vim.keymap.set("n", "<leader>wq", ":write<CR> :quit<CR>")
-vim.keymap.set("n", "<leader>h", ":help ")
+vim.keymap.set('n', '<leader>o', ':update<CR> :so<CR>')
+vim.keymap.set('n', '<leader>w', ':write<CR>')
+vim.keymap.set('n', '<leader>q', ':quit<CR>')
+vim.keymap.set('n', '<leader>wq', ':write<CR> :quit<CR>')
+vim.keymap.set('n', '<leader>h', ':help ')
 
-vim.keymap.set("n", "<leader>K", ":lua vim.lsp.buf.hover()<CR>")
-vim.o.winborder = "double"
+-- system clipboard
+vim.keymap.set({'n', 'v', 'x'}, '<leader>y', '"+y<CR>')
+vim.keymap.set({'n', 'v', 'x'}, '<leader>d', '"+d<CR>')
+vim.keymap.set({'n', 'v', 'x'}, '<leader>p', '"+p<CR>')
+
+-- highlight after yanking, from kickstart.nvim
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+vim.keymap.set('n', '<leader>K', ':lua vim.lsp.buf.hover()<CR>')
+vim.o.winborder = 'double'
 
 vim.opt.guicursor = table.concat({
-	"n-v-c:block",
-	"i-ci-ve:ver25-blinkwait700-blinkoff400-blinkon250",
-	"r-cr:hor20",
-	"o:hor50",
-	"sm:block-blinkwait175-blinkoff150-blinkon175"
-}, ",")
+	'n-v-c:block',
+	'i-ci-ve:ver25-blinkwait700-blinkoff400-blinkon250',
+	'r-cr:hor20',
+	'o:hor50',
+	'sm:block-blinkwait175-blinkoff150-blinkon175'
+}, ',')
 
 vim.pack.add({
-	{ src = "https://github.com/vague2k/vague.nvim" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/stevearc/oil.nvim" },
-	{ src = "https://github.com/mason-org/mason.nvim" },
-	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
-	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
+	{ src = 'https://github.com/vague2k/vague.nvim' },
+	{ src = 'https://github.com/neovim/nvim-lspconfig' },
+	{ src = 'https://github.com/stevearc/oil.nvim' },
+	{ src = 'https://github.com/mason-org/mason.nvim' },
+	{ src = 'https://github.com/mason-org/mason-lspconfig.nvim' },
+	{ src = 'https://github.com/chomosuke/typst-preview.nvim' },
 })
 
 -- manual update key
-vim.keymap.set("n", "<leader>pu", function()
+vim.keymap.set('n', '<leader>pu', function()
   if vim.pack and vim.pack.update then
     vim.pack.update()
   else
-    vim.notify("vim.pack.update() not available (need nightly)", vim.log.levels.WARN)
+    vim.notify('vim.pack.update() not available (need nightly)', vim.log.levels.WARN)
   end
-end, { desc = "Pack update" })
+end, { desc = 'Pack update' })
 
 -- run once on first launch of the day
-vim.api.nvim_create_autocmd("VimEnter", {
+vim.api.nvim_create_autocmd('VimEnter', {
   callback = function()
-    local ok, daily = pcall(require, "pack_daily")
+    local ok, daily = pcall(require, 'pack_daily')
     if ok and daily.maybe_update then daily.maybe_update() end
   end,
 })
 
-require "mason".setup()
-require "mason-lspconfig".setup({
-	ensure_installed = { "lua_ls", "basedpyright", "vtsls", "tinymist" },
+require 'mason'.setup()
+require 'mason-lspconfig'.setup({
+	ensure_installed = { 'lua_ls', 'basedpyright', 'vtsls', 'tinymist' },
 	automatic_installation = true
 })
-vim.lsp.enable({ "lua_ls" })
-vim.lsp.enable({ "basedpyright" })
-vim.lsp.enable({ "vtsls" })
-vim.lsp.enable({ "tinymist" })
-vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
+vim.lsp.enable({ 'lua_ls' })
+vim.lsp.enable({ 'basedpyright' })
+vim.lsp.enable({ 'vtsls' })
+vim.lsp.enable({ 'tinymist' })
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 
-require "oil".setup()
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+require 'oil'.setup()
+vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
 
-require "typst-preview".setup()
+require 'typst-preview'.setup()
 
-vim.cmd("colorscheme vague")
+vim.cmd('colorscheme vague')
